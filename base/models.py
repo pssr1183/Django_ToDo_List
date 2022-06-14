d2ba36a3e27530d16a6309573dbc0cc1c5f5a29f
pssr1183 <poornasaidarla@gmail.com>
2022-06-14 20:15:55 +0530

    after styling

--- base/models.py
+++ base/models.py
@@ -12,9 +12,3 @@ class Task(models.Model):
         return self.title
     class Meta:
         ordering = ['complete']
-class Employee(models.Model):
-    eid=models.CharField(max_length=20)
-    ename=models.CharField(max_length=100)
-    econtact=models.CharField(max_length=15)
-    class Meta:
-        db_table='employee'
